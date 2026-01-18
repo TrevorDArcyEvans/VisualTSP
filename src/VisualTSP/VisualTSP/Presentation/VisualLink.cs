0ef3f5a75b9febbbb7206b62068837c8244b6903
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2026-01-18 21:25:09 +0000

    - initial VisualLink

---
+++ src/VisualTSP/VisualTSP/Presentation/VisualLink.cs
@@ -0,0 +1,19 @@
+﻿namespace VisualTSP.Presentation;
+
+using Microsoft.UI.Xaml.Shapes;
+
+public sealed class VisualLink : Line
+{
+    public VisualLink()
+    {
+        //Canvas.ZIndex = "-1";
+        X1=25;
+        Y1=25;
+        X2=45;
+        Y2=45;
+        Tag = "42";
+        Stroke = "Black";
+        StrokeThickness = 4d;
+    }
+}
+
