6850a98964ef3637a6bfdbad9cd7292f7af5ffeb
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2026-01-18 21:38:55 +0000

    - added colour based highlighting

--- src/VisualTSP/VisualTSP/Presentation/VisualLink.cs
+++ src/VisualTSP/VisualTSP/Presentation/VisualLink.cs
@@ -1,5 +1,7 @@
 ﻿namespace VisualTSP.Presentation;
 
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Shapes;
 
 public sealed class VisualLink : Line
@@ -7,13 +9,32 @@ public sealed class VisualLink : Line
     public VisualLink()
     {
         //Canvas.ZIndex = "-1";
-        X1=25;
-        Y1=25;
-        X2=45;
-        Y2=45;
+        X1 = 25;
+        Y1 = 25;
+        X2 = 45;
+        Y2 = 45;
         Tag = "42";
         Stroke = "Black";
         StrokeThickness = 4d;
+        PointerEntered += Shape_OnMouseEntered;
+        PointerExited += Shape_OnMouseExited;
+    }
+
+    private Brush _oldColour;
+    private Brush _oldStroke;
+
+    private void Shape_OnMouseEntered(object sender, PointerRoutedEventArgs e)
+    {
+        var shape = (Shape) sender;
+        _oldColour = shape.Fill;
+        _oldStroke = shape.Stroke;
+        shape.Fill = shape.Stroke = new SolidColorBrush(Colors.Chartreuse);
+    }
+
+    private void Shape_OnMouseExited(object sender, PointerRoutedEventArgs e)
+    {
+        var shape = (Shape) sender;
+        shape.Fill = _oldColour;
+        shape.Stroke = _oldStroke;
     }
 }
-
--- src/VisualTSP/VisualTSP/Presentation/VisualNode.xaml.cs
+++ src/VisualTSP/VisualTSP/Presentation/VisualNode.xaml.cs
@@ -1,9 +1,31 @@
 ﻿namespace VisualTSP.Presentation;
 
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Shapes;
+
 public sealed partial class VisualNode : UserControl
 {
     public VisualNode()
     {
         InitializeComponent();
+    }
+    
+    private Brush _oldColour;
+    private Brush _oldStroke;
+
+    private void Shape_OnMouseEntered(object sender, PointerRoutedEventArgs e)
+    {
+        var shape = (Shape) sender;
+        _oldColour = shape.Fill;
+        _oldStroke = shape.Stroke;
+        shape.Fill = shape.Stroke = new SolidColorBrush(Colors.Chartreuse);
+    }
+
+    private void Shape_OnMouseExited(object sender, PointerRoutedEventArgs e)
+    {
+        var shape = (Shape) sender;
+        shape.Fill = _oldColour;
+        shape.Stroke = _oldStroke;
     }
 }
