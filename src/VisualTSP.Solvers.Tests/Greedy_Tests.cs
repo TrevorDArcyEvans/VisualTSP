787572a0ca2acccf4b8899f8d7d52a5613817c31
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2026-01-29 20:48:05 +0000

    - skeleton unit tests

---
+++ src/VisualTSP.Solvers.Tests/Greedy_Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace VisualTSP.Solvers.Tests;
+
+[TestFixture]
+public class Greedy_Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
