ec55f5cb77a43f665bb962ed62e3cfa8252c9ac4
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2026-01-29 15:52:40 +0000

    - align with JsonNetwork

--- src/Models/Network.cs
+++ src/Models/Network.cs
@@ -3,9 +3,9 @@ namespace VisualTSP.Models;
 public sealed class Network
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public required string Name { get; set; }
+    public string Name { get; set; }
     public List<Node> Nodes { get; set; } = [];
     public List<Link> Links { get; set; } = [];
-    public required Node Start { get; set; }
-    public required Node End { get; set; }
+    public Guid Start { get; set; }
+    public Guid End { get; set; }
 }
