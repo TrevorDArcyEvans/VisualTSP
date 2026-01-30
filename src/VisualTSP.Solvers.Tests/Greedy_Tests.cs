d56226fbd2383545fa9b459e6bdd29c602c83163
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2026-01-30 14:32:02 +0000

    - more unit tests

--- src/VisualTSP.Solvers.Tests/Greedy_Tests.cs
+++ src/VisualTSP.Solvers.Tests/Greedy_Tests.cs
@@ -34,6 +34,23 @@ public sealed class Greedy_Tests
         // aaa -> bbb -> ddd -> ccc -> eee
         _route.Count.ShouldBe(4);
 
+        // Note that direction of links is a bit random due to how they were initially defined
+        // TODO     make link start+end evaluation more robust
+
+        // aaa -> bbb
         _network.Nodes.Single(x => x.Id == _route[0].Start).Name.ShouldBe("aaa");
+        _network.Nodes.Single(x => x.Id == _route[0].End).Name.ShouldBe("bbb");
+
+        // bbb -> ddd
+        _network.Nodes.Single(x => x.Id == _route[1].Start).Name.ShouldBe("ddd");
+        _network.Nodes.Single(x => x.Id == _route[1].End).Name.ShouldBe("bbb");
+
+        // ddd -> ccc
+        _network.Nodes.Single(x => x.Id == _route[2].Start).Name.ShouldBe("ddd");
+        _network.Nodes.Single(x => x.Id == _route[2].End).Name.ShouldBe("ccc");
+
+        // ccc -> eee
+        _network.Nodes.Single(x => x.Id == _route[3].Start).Name.ShouldBe("eee");
+        _network.Nodes.Single(x => x.Id == _route[3].End).Name.ShouldBe("ccc");
     }
 }
